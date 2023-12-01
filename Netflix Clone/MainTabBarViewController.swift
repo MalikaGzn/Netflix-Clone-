de61a99e6d7ecb158d891ce4a03065da8db2dd3c
MalikaGzn <malikagozikhonova@gmail.com>
2023-12-01 16:54:13 +0500

    initialize

--- Netflix Clone/MainTabBarViewController.swift
+++ Netflix Clone/MainTabBarViewController.swift
@@ -11,7 +11,12 @@ class MainTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
+        view.backgroundColor = .systemYellow
+        
+        let vc1 = UINavigationController(rootViewController: HomeViewController())
+        let vc2 = UINavigationController(rootViewController: UpcomingViewController())
+        let vc3 = UINavigationController(rootViewController: SearchViewController())
+        let vc3 = UINavigationController(rootViewController: DownloadsViewController())
     }
 
 
