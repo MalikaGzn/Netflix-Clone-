0ad075c96ff0db963cda7927c7518bb488785230
MalikaGzn <malikagozikhonova@gmail.com>
2023-12-21 23:28:36 +0500

    tabbar upd

--- Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    private let homeFeedTable: UITableView = {
+        
+        //anonymys closure
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .red
-
+        view.backgroundColor = .systemBackground
+        // add that table view to general view
+        view.addSubview(homeFeedTable)
+    }
+    
+    //a frame so we can give it on screen
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        homeFeedTable.frame = view.bounds
     }
     
 }
