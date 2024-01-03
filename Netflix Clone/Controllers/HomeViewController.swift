827109787391a787b3dd4b2770bbaf258999a701
MalikaGzn <malikagozikhonova@gmail.com>
2024-01-04 01:35:13 +0500

    Collection controllers are added

--- Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -12,7 +12,7 @@ class HomeViewController: UIViewController {
     private let homeFeedTable: UITableView = {
         
         //anonymys closure, register custom View cell
-        let table = UITableView()
+        let table = UITableView(frame: .zero, style: .grouped)
         table.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
         return table
     }()
@@ -26,6 +26,9 @@ class HomeViewController: UIViewController {
         //two protocols to work the table properly
         homeFeedTable.delegate = self
         homeFeedTable.dataSource = self
+        
+        let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
+        homeFeedTable.tableHeaderView = headerView
     }
     
     //a frame so we can give it on screen
@@ -35,12 +38,20 @@ class HomeViewController: UIViewController {
     }
     
 }
+
+
 extension HomeViewController: UITableViewDelegate , UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
         return 20
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         guard let cell = tableView.dequeueReusableCell(withIdentifier: CollectionViewTableViewCell.identifier, for: indexPath) as? CollectionViewTableViewCell else {
             return UITableViewCell()
         }
